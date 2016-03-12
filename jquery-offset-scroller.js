a83670ef3cb6ab220178ebd440f33f6b5f27fa81
Mason Hale <masonhale@gmail.com>
2016-03-12 16:09:43 -0600

    initial commit

---
+++ jquery-offset-scroller.js
@@ -0,0 +1,47 @@
+// jQuery offsetScroller
+// Copyright (c) 2016 - Team Topia Inc.
+// Author: Mason Hale
+//
+// Provides handler to scroll smoothly to an #anchor
+//
+// Options:
+//     offsetPixels: 0     // useful for fixed headers
+//     animationSpeed: 500 // in milliseconds
+//
+(function($) {
+
+  function clickHandler( event ) {
+    var targetSel = $(this).attr("href");
+    // only support href's starting with '#'
+    if (targetSel && targetSel[0] === '#') {
+      var targetObj;
+      try {
+        targetObj = $(targetSel);
+      }
+      catch(e) {
+        console.log("offsetScroller could not scroll to: '" + targetSel + "'");
+      }
+      if (targetObj) {
+        var opts = event.data;
+        event.preventDefault();
+        $('html, body').animate({scrollTop: targetObj.offset().top - opts.offsetPixels}, opts.animationSpeed);
+        return false;
+      }
+    }
+  }
+
+  $.fn.offsetScroller = function( options ) {
+    var opts = $.extend( {}, $.fn.offsetScroller.defaults, options );
+
+    return this.each( function() {
+      $(this).bind( 'click', opts, clickHandler );
+    });
+  };
+
+  $.fn.offsetScroller.defaults = {
+    offsetPixels: 0,
+    animationSpeed: 500
+  }
+
+})(jQuery);
+
